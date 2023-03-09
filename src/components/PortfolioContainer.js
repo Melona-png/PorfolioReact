831a10caf39dd49f86fa048b4c119391224d3b14
Melona-png <118491178+Melona-png@users.noreply.github.com>
2023-03-09 00:24:09 -0500

    functional- need to style and add content, fixed minor console errors

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -3,6 +3,7 @@ import NavTabs from "./NavTabs";
 import About from "./pages/About";
 import Projects from "./pages/Projects"
 import Contact from "./pages/Contact";
+import Footer from "./Footer";
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("About");
@@ -26,6 +27,7 @@ export default function PortfolioContainer() {
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* // TODO: Add a comment explaining what is happening on the following line */}
       {renderPage()}
+     <Footer/> 
     </div>
   );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React from "react";
+import notetaker from "../images/notetaker.png";
 
 export default function Projects() {
+  // array goes here
+  const projectList = [
+    {
+      id: 1,
+      name: "JasonBorne",
+      description:
+        "Note Taker is a web application that allows you to write and save notes. It's built with Express.js on the back-end and provides an intuitive interface for organizing your thoughts and keeping track of tasks you need to complete.",
+      repo: "https://github.com/Melona-png/JSONBorne-NoteTaker",
+      deployed: "https://jsonborne.herokuapp.com/",
+      image: notetaker,
+    },
+    {
+      id: 2,
+      name: "WeatherAPI",
+      description: "lorem",
+      repo: "ipsum",
+      deployed: "weee",
+      image: "hello",
+    },
+  ];
   return (
     <div>
       <h1>Projects</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      {projectList.map((item) => (
+        <div className="card" style={{ width: "18rem" }} key={item.id}>
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            {/* fill in classNames with object info name,description,repo,deployed,image- like below */}
+            <h5 className="card-title">{item.name}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+          <div className="card-body">
+            <a href={item.repo} className="card-link">
+              Card link
+            </a>
+            <a href={item.deployed} className="card-link">
+              Another link
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
